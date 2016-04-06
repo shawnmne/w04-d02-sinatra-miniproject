b75e3421759ff6fc18e74e0f53da6a0a9ac4a2da
Shawn Michaud <shawnmne@hotmail.com>
2016-04-05 23:12:11 -0500

    all built

--- main.rb
+++ main.rb
@@ -1,4 +1,48 @@
 require "pry"
 require "sinatra"
 
-# Your routes go here:
+
+get '/home' do
+	erb :home
+end
+
+get '/temp-convert' do 
+	erb :temp_convert
+end
+
+get '/temp-convert-results' do
+	@temperature = params['temperature'].to_i
+	@units = params['units']
+	erb :temp_convert_results
+end
+
+get '/reverser' do 
+	erb :reverser
+end
+
+get '/reverser-results' do 
+	@phrase = params['phrase']
+	erb :reverser_results
+end	
+
+def convert_the_temp(temp, unit)
+	if unit == "f"
+		temp = ((temp - 32) * 5) / 9
+	elsif unit == "c"
+		temp = (temp * 9 ) / 5  + 32 
+	end
+	return temp
+end
+
+def convert_the_unit(unit)
+	if unit == "f"
+		unit = "c"
+	elsif unit == "c"
+		unit = "f"
+	end
+end
+
+def reverse_the_phrase(phrase)
+	phrase.reverse
+end
+
